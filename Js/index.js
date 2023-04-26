411f53fe88b301bfbc4b1fd2f922057c9a32d155
Gss92 <gsansousa92@gmail.com>
2023-04-26 19:09:40 -0300

    Adição de máscara no campo de telefone

--- Js/index.js
+++ Js/index.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
 
+    $("#telephone").mask("(00)00000-0000",{
+        placeholder: "(00)00000-0000"
+    })
     $("#cpf").mask("000.000.000-00",{
         placeholder: "000.000.000-000"
     })
